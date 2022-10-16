df33cec0770c9c42db2a3acd01b30034d3653adb
chrisean nichols <chriseannichols550@gmail.com>
2022-10-16 12:33:04 -0400

    added email services

--- api/emailer/index.js
+++ api/emailer/index.js
@@ -1,43 +1,51 @@
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-
 const template = require('./emailTemplate');
-const sgMail = require('@sendgrid/mail')
 const axios = require('axios');
+const { EmailClient } = require('@azure/communication-email');
+const verifyURL = 'https://www.google.com/recaptcha/api/siteverify';
+const connectionString = process.env.COMMUNICATION_SERVICES_CONNECTION_STRING;
+const captchaSecret = process.env.RECAPTCHA_SECRET_KEY;
 
 module.exports = async function(context, req) {
     const {name, email, message, token} = req.body;
-    const verifyURL = 'https://www.google.com/recaptcha/api/siteverify'
-
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-        to: 'chriseannichols550@gmail.com',
-        from: 'chriseannichols550@gmail.com',
-        subject: 'Website Form Submission',
-        text: template(name, email, message)
+    const params = `?secret=${captchaSecret}&response=${token}`;
+    const emailConfig = {
+        sender: 'DoNotReply@9b0c6868-9c83-41c5-b16a-8e77cd432019.azurecomm.net',
+        content: {
+            subject: 'Website Form Submission',
+            plainText: template(name, email, message)
+        },
+        recipients: {
+            to: [
+                {
+                    email:'chriseannichols550@gmail.com'
+                }
+            ]
+        }
     }
     
     try{
-        const res = await axios.post(verifyURL,{
-            secret: process.env.RECAPTCHA_SECRET_KEY,
-            response: token
-        })
-        if(res.success){
-            try {
-                await sgMail.send(msg)
-                console.log('Email sent successfully');
-                context.res.json({
-                    body: 200,
-                    text: 'Email sent successfully'
-                });
-            } catch(error) {
-                console.error('There was an issue sending the email');
-                console.error(error);
-                context.res.json({
-                    body: 500,
-                    text: 'There was an error sending the message'
-                });
+        const captchaRes = await axios.post(
+            verifyURL+params,
+            {},
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"   
+                }
             }
+        )
+        if(captchaRes.data.success){
+            try {
+                const client = new EmailClient(connectionString);
+                await client.send(emailConfig);
+            } catch(error) {
+                console.error(error);
+            }
+        } else {
+            console.log('Captcha failed.')
+            context.res.json({
+                body: 500,
+                text: 'The captcha did not pass'
+            });
         }
     } catch(error) {
         console.error('Captcha threw an error');
